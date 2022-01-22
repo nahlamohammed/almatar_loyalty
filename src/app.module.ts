23dba2cea1a4273aa7a0387e87ed554880845ddb
nahlazaki <eng.nahla.mohammed@gmail.com>
2022-01-22 13:48:32 +0200

    remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,5 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { TransfersModule } from './transfers/transfers.module';
@@ -9,7 +7,5 @@ import { TransfersModule } from './transfers/transfers.module';
 
 @Module({
   imports: [MongooseModule.forRoot('mongodb://127.0.0.1:27017/almatarLoyality'), UsersModule, AuthModule, TransfersModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
